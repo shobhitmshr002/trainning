defabb5c78aaa7af43cd967f51104742a1c0735e
shobhitmshr002 <shobhitmshr002@gmail.com>
2013-09-09 15:49:27 +0530

    commit

---
+++ find.js
@@ -0,0 +1,21 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: manusis
+ * Date: 9/3/13
+ * Time: 11:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+function fnr()
+{
+    var x=document.getElementById("text");
+    var find=document.getElementById("find");
+    var replace=document.getElementById("replace");
+
+
+           x.replace(/ss/gi,replace);
+           return false;
+
+}
---
+++ newfind.js
@@ -0,0 +1,38 @@
+function find()
+{
+    var a=document.getElementById("txtf").value;
+    var b=document.getElementById("ta").value;
+    var c="";
+    for(var i=0;i<b.length;i++)
+    {
+        var d=b.substr(i,a.length)
+        if (d.indexOf(a) > -1)
+            c=c +" " + (d.indexOf(a)+i);
+    }
+    if (c!="")
+    {
+        alert(c);
+    }
+    else
+    {
+        alert("not find");
+    }
+}
+function replace()
+{
+    var a=document.getElementById("txtf").value;
+    var b=document.getElementById("ta").value;
+    var c="";
+    for(var i=0;i<b.length;i++)
+    {
+        var d=b.substr(i,a.length)
+        if (d.indexOf(a) > -1)
+        {
+            c=c + document.getElementById("txtr").value;
+            i=i+a.length-1;
+        }
+        else
+            c=c + b.charAt(i);
+    }
+    document.getElementById("ta").value=c;
+}
